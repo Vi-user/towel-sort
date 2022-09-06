18904eb785410422dfcfd6c21d6daf923d42faa5
Vi-user <vi.volkova.2712@gmail.com>
2022-09-06 10:33:07 +0300

    refactor: modified task solution

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@
 
 module.exports = function towelSort (matrix) {
     let res = []
-    if (!matrix) return res;
-    if (!matrix.length) return res;
+    if (!matrix || !matrix.length) return res;
     for (let i = 0; i < matrix.length; i++) {
-        if (!(i % 2)) {
-            res.push(matrix[i])
-        } else {
-            res.push(matrix[i].reverse())
-        }
+        (!(i % 2)) ? res.push(matrix[i]) : res.push(matrix[i].reverse());
     }
-  return res.join(',').split(',');
+    return res.flat();
 }
